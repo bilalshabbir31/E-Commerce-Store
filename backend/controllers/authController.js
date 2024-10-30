4673d977cfb0a337a48961d73c9226227319e9cf
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-30 16:58:30 +0400

    add signup action

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,23 @@
-export const signup = (req, res) => {
-  res.send("Sign-up");
+import User from "../models/user.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already Exists." });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({ user, message: "User created Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = (req, res) => {
