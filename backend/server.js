c75b028378ba9fc8d8c6b464dc6635a076ffef21
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-30 15:09:40 +0400

    Initialize server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:"+PORT);
+});
