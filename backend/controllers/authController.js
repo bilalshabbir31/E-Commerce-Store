4e33ad170f7a6a1399fcf760e00ce41144325bc2
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-30 17:42:42 +0400

    complete signup action

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,16 @@
+import { redis } from "../config/redis.js";
 import User from "../models/user.js";
+import { setCookies } from "../utils/cookies.js";
+import generateTokens from "../utils/generateToken.js";
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60 * 1000
+  ); // 7 days
+};
 
 export const signup = async (req, res) => {
   try {
@@ -14,7 +26,21 @@ export const signup = async (req, res) => {
       password,
     });
 
-    res.status(201).json({ user, message: "User created Successfully" });
+    // authenticate user
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "User created Successfully",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
