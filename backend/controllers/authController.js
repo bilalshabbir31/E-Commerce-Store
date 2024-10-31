e3942230e911bc1d4f5b0b533672be625d9c2e73
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-10-31 09:54:09 +0400

    complete the login functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ import { redis } from "../config/redis.js";
 import User from "../models/user.js";
 import { setCookies } from "../utils/cookies.js";
 import generateTokens from "../utils/generateToken.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const storeRefreshToken = async (userId, refreshToken) => {
   await redis.set(
@@ -47,8 +47,28 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login");
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    console.log(await user.comparePassword(password));
+    
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Email or Password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
