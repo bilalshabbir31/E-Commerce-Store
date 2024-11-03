d0c6baddea1cd3aac196e7b1b1024cc836b130e4
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-03 11:08:44 +0400

    add store

---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useUserStore = create((set, get) => ({
+  user: null,
+  loading: false,
+  checkingAuth: true,
+
+  signup: async ({ name, email, password, confirmPassword }) => {
+    set({ loading: true });
+    if (password !== confirmPassword) {
+      set({ loading: false });
+      return toast.error("Password do not match");
+    }
+
+    try {
+      const res = await axios.post("/auth/signup", { name, email, password });
+      set({ user: res.data, loading: false });
+    } catch (error) {
+      toast.error(error.response.data.message || "An Error Occurred");
+    }
+  },
+  login: async (email, password) => {
+    set({ loading: true });
+
+    try {
+      const res = await axios.post("/auth/login", { email, password });
+      set({ user: res.data, loading: false });
+    } catch (error) {
+      toast.error(error.response.data.message || "An Error Occurred");
+    }
+  },
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+    try {
+      const response = await axios.get("/auth/profile");
+      set({ user: response.data, checkingAuth: false });
+    } catch (error) {
+      set({
+        user: null,
+        checkingAuth: false,
+      });
+    }
+  },
+}));
