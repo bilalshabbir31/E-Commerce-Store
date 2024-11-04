b5417ccf9cdbfe9e48cfd6a487fdf973a7a75577
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-04 16:32:00 +0400

    add product store

---
+++ frontend/src/stores/useProductStore.js
@@ -0,0 +1,77 @@
+import axios from "../lib/axios";
+import { create } from "zustand";
+import toast from "react-hot-toast";
+
+export const useProductStore = create((set) => ({
+  products: [],
+  loading: false,
+  setProduct: (product) => set({ product }),
+  createProduct: async (productData) => {
+    set({ loading: true });
+    try {
+      const res = await axios.post("/products", productData);
+      set((prevState) => ({
+        products: [...prevState.products, res.data.product],
+        loading: false,
+      }));
+    } catch (error) {
+      toast.error(error.response.data.error);
+      set({ loading: false });
+    }
+  },
+  fetchAllProducts: async () => {
+    set({ loading: true });
+    try {
+      const res = await axios.get("/products");
+      set({ products: res.data.products, loading: false });
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response.data.error);
+    }
+  },
+  fetchProductsByCategory: async (category) => {
+    set({ loading: true });
+    try {
+      const res = await axios.get(`products/category/${category}`);
+      console.log(res.data.products);
+      
+      set({ products: res.data.products, loading: false });
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response.data.error);
+    }
+  },
+  deleteProduct: async (productId) => {
+    set({ loading: true });
+    try {
+      await axios.delete(`/products/${productId}`);
+      set((prevProducts) => ({
+        products: prevProducts.products.filter(
+          (product) => product._id !== productId
+        ),
+        loading: false,
+      }));
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response.data.error || "Failed to delete product");
+    }
+  },
+  toggleFeaturedProduct: async (productId) => {
+    set({ loading: true });
+    try {
+      const response = await axios.patch(`/products/${productId}`);
+      // this will update the isFeatured prop of the product
+      set((prevProducts) => ({
+        products: prevProducts.products.map((product) =>
+          product._id === productId
+            ? { ...product, isFeatured: response.data.isFeatured }
+            : product
+        ),
+        loading: false,
+      }));
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response.data.error || "Failed to update product");
+    }
+  },
+}));
