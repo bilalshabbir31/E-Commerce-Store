f7788e6f53302420a9db26e732f3cfc2aea23ee5
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-05 09:07:05 +0400

    fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,8 +20,9 @@ function App() {
   }, [checkAuth])
 
   useEffect(() => {
+    if (!user) return;
     getCartItems()
-  }, [getCartItems])
+  }, [getCartItems, user])
 
   if (checkingAuth) return <LoadingSpinner />
 
