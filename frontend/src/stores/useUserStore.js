e01c0faef357695772e7d9f6d3add76f78ab1145
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-06 13:05:48 +0400

    access token to refresh using refresh token in background

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -51,4 +51,45 @@ export const useUserStore = create((set, get) => ({
       toast.error(error.response.data.message || "An Error Occurred");
     }
   },
+  refreshToken: async () => {
+    if (get().checkingAuth) return;
+    set({ checkingAuth: true });
+    try {
+      const response = await axios.post("auth/refresh-token");
+      set({ checkingAuth: false });
+      return response.data;
+    } catch (error) {
+      set({ user: null, checkingAuth: false });
+      throw error;
+    }
+  },
 }));
+
+let refreshPromise = null;
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error?.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        if (refreshPromise) {
+          await refreshPromise;
+          return axios(originalRequest);
+        }
+
+        // start a new refresh process
+        refreshPromise = useUserStore.getState().refreshToken();
+        await refreshPromise;
+        refreshPromise = null;
+        return axios(originalRequest);
+      } catch (refreshError) {
+        // if refresh fails redirect to login or handle as needed
+        useUserStore.getState().logout();
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
